e3ece539cc18bca938fba56e53fc5c8ca56d68eb
Mark T. Holder <mtholder@gmail.com>
2015-07-27 11:48:53 -0400

    dealing with new paup output format

--- scripts/mtdna/sitelnL2TabDelimited.py
+++ scripts/mtdna/sitelnL2TabDelimited.py
@@ -2,27 +2,32 @@ import sys
 inp = open(sys.argv[1])
 inp.next()
 offset = 0
-row_lnL = [['tree1']]
+tree2_sitelnL = {}
+tree_names = []
 for row in inp:
+    rs = row.strip()
+    if not rs:
+        continue
     s = row.split('\t')
+    #print s
     if s[0]:
-        offset += 1
-        r_iter = iter(row_lnL)
-        r = r_iter.next()
-        r.append('tree%d' % (1 + offset))
+        if len(tree_names) > 1:
+            assert len(row_lnL) == num_sites
+        elif len(tree_names) == 1:
+            num_sites = len(row_lnL)
+        row_lnL = []
+        tree_name = 'tree{}'.format(1 + len(tree_names))
+        tree_names.append(tree_name)
+        tree2_sitelnL[tree_name] = row_lnL
     else:
         last_word = s[-1].strip()
-        if offset == 0:
-            row_lnL.append([last_word])
-        else:
-            r = r_iter.next()
-            assert(len(r) == offset)
-            r.append(last_word)
+        row_lnL.append('-' + last_word)
 
-for n, r in enumerate(row_lnL):
-    if n == 0:
-        sys.stdout.write('Site\t')
-    else:
-        sys.stdout.write('%d\t' % (n))
-    print '\t'.join(r)
-sys.stderr.write(str(offset) + ' trees\n')
+sys.stdout.write('Site\t{}\n'.format('\t'.join(tree_names)))
+site_number = 1
+for site_number in range(1, num_sites + 1):
+    sys.stdout.write('{}'.format(site_number))
+    for tn in tree_names:
+        siteLnL = tree2_sitelnL[tn][site_number - 1]
+        sys.stdout.write('\t{}'.format(siteLnL))
+    sys.stdout.write('\n')
